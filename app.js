7a2ac5022a0455734b6adc942db9d1d264c56d7c
karsho9614 <karsho9614@gmail.com>
2022-02-16 23:14:50 +0900

    changeType

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 var server = http.createServer(
     (request,response)=>{
         fs.readFile('./index.html','UTF-8',(error,data)=>{
-            response.writeHead(200, {'Content-Type':'text/html'});
+            response.writeHead(200, {'Content-Type':'text/css'});
             response.write(data);
             response.end();
         })
