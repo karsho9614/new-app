9cca3f10eb5353ae03547ad8040670280e47ae43
karsho9614 <karsho9614@gmail.com>
2022-02-16 23:19:35 +0900

    remod

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 var server = http.createServer(
     (request,response)=>{
         fs.readFile('./index.html','UTF-8',(error,data)=>{
-            response.writeHead(200, {'Content-Type':'text/css'});
+            response.writeHead(200, {'Content-Type':'text/html'});
             response.write(data);
             response.end();
         })
