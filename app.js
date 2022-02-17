6cc7583b49b7a9ad443194b59f4ed76986d0ca1d
karsho9614 <karsho9614@gmail.com>
2022-02-17 22:22:54 +0900

    contentType修正

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 const http = require('http');
 const fs = require('fs');
 
+var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.wasm': 'application/wasm'
+};
+
+var contentType = mimeTypes[extname] || 'application/octet-stream';
+
 var server = http.createServer(
     (request,response)=>{
         fs.readFile('./index.html','UTF-8',(error,data)=>{
-            response.writeHead(200, {'Content-Type':'text/html'});
+            response.writeHead(200, {'Content-Type':contentType});
             response.write(data);
             response.end();
         })
